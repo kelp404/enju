102ba2d8690ee360fab8a7abb237f32b52973f1b
Kelp <kelp@phate.org>
2015-09-07 15:53:24 +0800

    removed debug code.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 (function() {
-  var ProductModel, UserModel, enju, exceptions, properties,
-    extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-    hasProp = {}.hasOwnProperty;
+  var exceptions, properties;
 
   exports.Document = require('./lib/document');
 
@@ -34,76 +32,5 @@
   exports.OperationError = exceptions.OperationError;
 
   exports.SyntaxError = exceptions.SyntaxError;
-
-  enju = require('./');
-
-  UserModel = (function(superClass) {
-    extend(UserModel, superClass);
-
-    function UserModel() {
-      return UserModel.__super__.constructor.apply(this, arguments);
-    }
-
-    UserModel._index = 'test_users';
-
-    UserModel._settings = {
-      analysis: {
-        analyzer: {
-          email_url: {
-            type: 'custom',
-            tokenizer: 'uax_url_email'
-          }
-        }
-      }
-    };
-
-    UserModel.define({
-      name: new enju.StringProperty({
-        required: true
-      }),
-      email: new enju.StringProperty({
-        required: true,
-        analyzer: 'email_url'
-      }),
-      createTime: new enju.DateProperty({
-        autoNow: true,
-        dbField: 'create_time'
-      })
-    });
-
-    return UserModel;
-
-  })(enju.Document);
-
-  ProductModel = (function(superClass) {
-    extend(ProductModel, superClass);
-
-    function ProductModel() {
-      return ProductModel.__super__.constructor.apply(this, arguments);
-    }
-
-    ProductModel._index = 'test_products';
-
-    ProductModel.define({
-      user: new enju.ReferenceProperty({
-        referenceClass: UserModel,
-        required: true
-      }),
-      title: new enju.StringProperty({
-        required: true
-      }),
-      createTime: new enju.DateProperty({
-        autoNow: true,
-        dbField: 'create_time'
-      })
-    });
-
-    return ProductModel;
-
-  })(enju.Document);
-
-  ProductModel.get('AU-mAh1-trhIjlPeQBbM').then(function(product) {
-    return console.log(product);
-  });
 
 }).call(this);
